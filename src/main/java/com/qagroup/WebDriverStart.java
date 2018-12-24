0cb68d3ca027674d56ab46da266f866f26e12038
schkred <schkredt@gmail.com>
2018-12-24 19:26:28 +0200

    24 Dec 18

--- src/main/java/com/qagroup/WebDriverStart.java
+++ src/main/java/com/qagroup/WebDriverStart.java
@@ -1,5 +1,13 @@
 package com.qagroup;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class WebDriverStart {
 
+	public static void main(String[] args) {
+//		System.setProperty("webdriver.gecko.driver", "C:\\Users\\SYurivych\\eclipse-workspace\\WebDrivers\\geckodriver.exe");
+		FirefoxDriver ffdriver = new FirefoxDriver();
+		ffdriver.get("https://www.phptravels.net/");
+		
+	}
 }
