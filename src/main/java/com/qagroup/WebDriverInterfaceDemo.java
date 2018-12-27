6f46f5a6f78f534a2a2b327ef46cf273939db582
schkred <schkredt@gmail.com>
2018-12-27 23:02:36 +0200

    27 Dec 18

---
+++ src/main/java/com/qagroup/WebDriverInterfaceDemo.java
@@ -0,0 +1,16 @@
+package com.qagroup;
+
+import java.util.Random;
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverInterfaceDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static WebDriver startWebDriver() {
+		int rand = new Random().nextInt(2);
+		
+	}
+}
