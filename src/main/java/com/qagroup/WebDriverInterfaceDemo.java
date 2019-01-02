3da03cbc62d26d9e3bac8202b021db5b1cb9e3dd
schkred <schkredt@gmail.com>
2019-01-02 18:17:16 +0200

    28 Dec 18

--- src/main/java/com/qagroup/WebDriverInterfaceDemo.java
+++ src/main/java/com/qagroup/WebDriverInterfaceDemo.java
@@ -1,16 +1,29 @@
 package com.qagroup;
 
-import java.util.Random;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.github.javafaker.Faker;
 
 public class WebDriverInterfaceDemo {
 
 	public static void main(String[] args) {
+		WebDriver driver = startWebDriver();
+		driver.get("https://www.phptravels.net/");
+
+		// System.out.println(new Faker().chuckNorris().fact());
 
 	}
 
 	public static WebDriver startWebDriver() {
-		int rand = new Random().nextInt(2);
-		
+		int rand = new Faker().number().numberBetween(0, 1);
+		switch (rand) {
+		case 0:
+			return new ChromeDriver();
+		default:
+			return new FirefoxDriver();
+		}
+
 	}
 }
