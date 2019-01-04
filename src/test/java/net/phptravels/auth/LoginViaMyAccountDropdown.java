5168eb6f86b49f783655339b982fbc19643d75e3
schkred <schkredt@gmail.com>
2019-01-04 19:28:40 +0200

    3 Jan 19

--- src/test/java/net/phptravels/auth/LoginViaMyAccountDropdown.java
+++ src/test/java/net/phptravels/auth/LoginViaMyAccountDropdown.java
@@ -3,6 +3,7 @@ package net.phptravels.auth;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
@@ -26,6 +27,28 @@ public class LoginViaMyAccountDropdown {
 		
 		WebElement loginOption = myAccountDropdown.findElement(By.xpath(".//a[contains(text(), 'Login')]"));
 		loginOption.click();
+		
+		waitFor(2);
+		
+		WebElement usernameField = driver.findElement(By.name("username"));
+		usernameField.sendKeys("user@phptravels.com");
+		WebElement passwordField = driver.findElement(By.name("password"));
+		passwordField.sendKeys("demouser");
+		WebElement loginButton = driver.findElement(By.cssSelector(".loginbtn"));
+		loginButton.click();
+		
+		waitFor(2);
+		
+		String expectedURL = "https://www.phptravels.net/account/";
+		String actualURL = driver.getCurrentUrl();
+		
+		Assert.assertEquals(actualURL, expectedURL, "Incorrect URL:");
+		
+		WebElement userNameOnHeader = driver.findElement(By);
+		
+		String actualUserNameOnHeader = myAccountButton.getText();
+		
+		Assert.assertEquals(actualUserNameOnHeader, "JOHNY", "Incorrect username: ");
 	}
 	
 	@AfterMethod(alwaysRun = true)
@@ -33,5 +56,15 @@ public class LoginViaMyAccountDropdown {
 		if (driver != null) {
 			driver.quit();
 		}
+	}
+	
+	private void waitFor(int seconds) {
+		try {
+			Thread.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
