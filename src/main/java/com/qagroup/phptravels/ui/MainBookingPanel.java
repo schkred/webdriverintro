274294a5c3686b34c4e74e86b30a0759d6992af3
schkred <schkredt@gmail.com>
2019-03-10 22:18:22 +0200

    10 Mar 19

--- src/main/java/com/qagroup/phptravels/ui/MainBookingPanel.java
+++ src/main/java/com/qagroup/phptravels/ui/MainBookingPanel.java
@@ -10,77 +10,123 @@ import io.qameta.allure.Step;
 
 public class MainBookingPanel {
 
-    private SearchContext root;
+	private SearchContext root;
 
-    @FindBy(css = "[data-title='hotels']")
-    private WebElement hotelsTab;
+	@FindBy(css = "[data-title='hotels']")
+	private WebElement hotelsTab;
+	
+	@FindBy(css = "select2-choice")
+	private WebElement searchByHotelOrCityInput;
 
-    @FindBy(css = "[name=checkin]")
-    private WebElement checkInField;
+	@FindBy(css = "[name=checkin]")
+	private WebElement checkInField;
 
-    @FindBy(css = "[name=checkout]")
-    private WebElement checkOutField;
+	@FindBy(css = "[name=checkout]")
+	private WebElement checkOutField;
 
-    @FindBy(id = "travellersInput")
-    private WebElement adultsAndChildrenField;
+	@FindBy(id = "travellersInput")
+	private WebElement adultsAndChildrenField;
 
-    @FindBy(id = "adultMinusBtn")
-    private WebElement minusAdultsButton;
+	@FindBy(id = "adultMinusBtn")
+	private WebElement minusAdultsButton;
 
-    @FindBy(id = "adultPlusBtn")
-    private WebElement plusAdultsButton;
+	@FindBy(id = "adultPlusBtn")
+	private WebElement plusAdultsButton;
 
-    @FindBy(id = "childMinusBtn")
-    private WebElement minusChildrenButton;
+	@FindBy(id = "childMinusBtn")
+	private WebElement minusChildrenButton;
 
-    @FindBy(id = "childPlusBtn")
-    private WebElement plusChildrenButton;
+	@FindBy(id = "childPlusBtn")
+	private WebElement plusChildrenButton;
 
-    @FindBy(id = "adultInput")
-    private WebElement numberOfAdultsInput;
+	@FindBy(id = "adultInput")
+	private WebElement numberOfAdultsInput;
 
-    @FindBy(id = "childInput")
-    private WebElement numberOfChildrenInput;
+	@FindBy(id = "childInput")
+	private WebElement numberOfChildrenInput;
 
-    public MainBookingPanel(WebElement mainBookingPanel) {
-        this.root = mainBookingPanel;
-        PageFactory.initElements(new DefaultElementLocatorFactory(root), this);
-    }
+	public MainBookingPanel(WebElement mainBookingPanel) {
+		this.root = mainBookingPanel;
+		PageFactory.initElements(new DefaultElementLocatorFactory(root), this);
+	}
 
-    @Step("Select 'Hotels' tab on Main booking panel")
-    public MainBookingPanel selectHotelsTab() {
-        hotelsTab.click();
-        return this;
-    }
+	@Step("Select 'Hotels' tab on Main booking panel")
+	public MainBookingPanel selectHotelsTab() {
+		hotelsTab.click();
+		return this;
+	}
+	
+	public void searchByHotelOrCity(String string) {
+		searchByHotelOrCityInput.click();
+		searchByHotelOrCityInput.sendKeys("Dubai");
+		
+	}
 
-    @Step("Open 'Check in' date picker")
-    public void openHotelsCheckInDatePicker() {
-        checkInField.click();
-    }
+	@Step("Open 'Check in' date picker")
+	public void openHotelsCheckInDatePicker() {
+		checkInField.click();
+	}
 
-    @Step("Open 'Check out' date picker")
-    public void openHotelsCheckOutDatePicker() {
-        checkOutField.click();
-    }
+	@Step("Open 'Check out' date picker")
+	public void openHotelsCheckOutDatePicker() {
+		checkOutField.click();
+	}
 
-    @Step("Open 'Adults and children' picker")
-    public void openAdultsAndChildrenPicker() {
-        adultsAndChildrenField.click();
-    }
+	@Step("Open 'Adults and children' picker")
+	public void openAdultsAndChildrenPicker() {
+		adultsAndChildrenField.click();
+	}
 
-    @Step("Set the number of adults and children")
-    public void selectAdultsAndChildren(int adults, int children) {
-        setNumberOfAdults(adults);
-        setNumberOfChildren(children);
-    }
+	@Step("Set the number of adults and children")
+	public void selectAdultsAndChildren(int adults, int children) {
+		setNumberOfAdults(adults);
+		setNumberOfChildren(children);
+	}
 
-    private void setNumberOfAdults(int adults) {
-        numberOfAdultsInput.clear();
-        numberOfAdultsInput.sendKeys(Integer.toString(adults));
-    }
+//    private void setNumberOfAdults(int adults) {
+//        numberOfAdultsInput.clear();
+//        numberOfAdultsInput.sendKeys(Integer.toString(adults));
+//    }
 
-    private void setNumberOfChildren(int children) {
-        numberOfChildrenInput.clear();
-        numberOfChildrenInput.sendKeys(Integer.toString(children));
-    }
+	private void setNumberOfAdults(int adults) {
+		int currentValue = Integer.valueOf(numberOfAdultsInput.getAttribute("value"));
+		if (currentValue == adults) {
+			return;
+		}
+		if (currentValue < adults) {
+			while (currentValue != adults) {
+				plusAdultsButton.click();
+				currentValue = Integer.valueOf(numberOfAdultsInput.getAttribute("value"));
+			}
+		}
+		if (currentValue > adults) {
+			while (currentValue != adults) {
+				minusAdultsButton.click();
+				currentValue = Integer.valueOf(numberOfAdultsInput.getAttribute("value"));
+			}
+		}
+	}
+//
+//    private void setNumberOfChildren(int children) {
+//        numberOfChildrenInput.clear();
+//        numberOfChildrenInput.sendKeys(Integer.toString(children));
+//    }
+	private void setNumberOfChildren(int children) {
+		int currentValue = Integer.valueOf(numberOfChildrenInput.getAttribute("value"));
+		if (currentValue == children) {
+			return;
+		}
+		if (currentValue < children) {
+			while (currentValue != children) {
+				plusChildrenButton.click();
+				currentValue = Integer.valueOf(numberOfAdultsInput.getAttribute("value"));
+			}
+		}
+		if (currentValue > children) {
+			while (currentValue != children) {
+				minusChildrenButton.click();
+				currentValue = Integer.valueOf(numberOfAdultsInput.getAttribute("value"));
+			}
+		}
+	}
 }
--- src/test/java/net/phptravels/booking/BookHotelByCityNameTest.java
+++ src/test/java/net/phptravels/booking/BookHotelByCityNameTest.java
@@ -14,23 +14,35 @@ public class BookHotelByCityNameTest {
 	private MainPage mainPage;
 	private DatePicker datePicker;
 	private MainBookingPanel mainBookingPanel;
-	
+
 	@Test
 	public void testBookingHotelByCityName() {
 		mainPage = phpTtravelsApp.openMainPage();
 		mainBookingPanel = mainPage.getMainBookingPanel();
+		
+		mainBookingPanel.searchByHotelOrCity("");
+		
 		mainBookingPanel.selectHotelsTab().openHotelsCheckInDatePicker();
 		datePicker = mainPage.getDatePicker();
-		
+
 		LocalDate checkInDate = LocalDate.now().plusYears(1).plusMonths(1);
 		datePicker.selectDate(checkInDate);
-		
-		mainPage.getMainBookingPanel().openHotelsCheckOutDatePicker();
-		LocalDate checkOutDate = checkInDate.plusDays(3);
+
+		LocalDate checkOutDate = checkInDate.plusMonths(2);
 		datePicker.selectDate(checkOutDate);
-		
+
 		mainBookingPanel.openAdultsAndChildrenPicker();
-		mainBookingPanel.selectAdultsAndChildren(4, 2);
-		
+		mainBookingPanel.selectAdultsAndChildren(6, 4);
+
+	}
+
+	private void waitFor(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
